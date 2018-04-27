77fe32fa67063e793bda4d31ef900d5e11c5e4de
ChaacNado <35525138+ChaacNado@users.noreply.github.com>
2018-04-27 12:59:52 +0200

    La till en tom CharacterManager

---
+++ PixelFighters/PixelFighters/Managers/CharacterManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PixelFighters
+{
+    class CharacterManager
+    {
+        ///Med denna kan vi byta ut texturer, attacklogik osv på spelarens karaktär
+        ///Denna bör nog enbart modifera och nås av Player eller möjligtvis MovingObject
+    }
+}
